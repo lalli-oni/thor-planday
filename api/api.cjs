516237733b97cbb46fe6fcd09d48fb72e8d0a7d5
lalli-oni <lalli.oni@gmail.com>
2025-02-11 12:05:32 +0100

    Use cors Return data.json

--- api/api.cjs
+++ api/api.cjs
@@ -1,9 +1,15 @@
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
 const port = 3000
 
+const data = require('./data.json')
+
+app.use(cors())
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(data)
 })
 
 app.listen(port, () => {
