c3b784ca456accbce0182bcd0bdf63f00af3d85e
lalli-oni <lalli.oni@gmail.com>
2025-02-12 14:17:04 +0100

    Inline the api query into <Table /> Use Infinite Query Add pagination support

--- src/Table.tsx
+++ src/Table.tsx
@@ -1,13 +1,67 @@
-import { useThings } from './api'
+import { useCallback, useMemo, useState } from 'react'
 
-import ThingsTable from './ThingsTable'
-
+import ThingsTable, { type FetchDirection } from './ThingsTable'
+import { useInfiniteQuery } from '@tanstack/react-query'
 
 function Table() {
-  const { data, error, isFetching } = useThings()
+  const [pageIndex, setPageIndex] = useState<number>(0)
+  
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    fetchPreviousPage,
+    hasPreviousPage,
+    isFetching,
+  } = useInfiniteQuery({
+    queryKey: ['thing'],
+    retry: false,
+    initialPageParam: null,
+    queryFn: async ({ pageParam, direction }) => {
+      const host = 'http://localhost:3000'
+      let response
+      if (!pageParam) {
+        response = await fetch(host)
+      } else {
+        const queryParams = new URLSearchParams()
+        queryParams.append(direction === 'forward' ? 'startCursor' : 'endCursor', pageParam)
+        response = await fetch(`${host}?${queryParams.toString()}`)
+      }
+      return await response.json()
+    },
+    getNextPageParam: (lastPage) => lastPage.meta.nextCursor,
+    getPreviousPageParam: (firstPage) => firstPage.meta.previousCursor,
+  })
+
+  const onPaginationChange = useCallback((direction: FetchDirection) => {
+    if (direction === 'forward') {
+      const newIndex = pageIndex + 1
+      setPageIndex(newIndex)
+      if (!data?.pages[newIndex]) fetchNextPage()
+    } else {
+      const newIndex = pageIndex - 1
+      setPageIndex(newIndex)
+      if (!data?.pages[newIndex]) fetchPreviousPage()
+    }
+  }, [fetchNextPage, fetchPreviousPage, pageIndex, data?.pages])
+
+  const pagination = useMemo(() => {
+    return {
+      hasNextPage: hasNextPage || data?.pages[pageIndex + 1],
+      hasPreviousPage: hasPreviousPage || data?.pages[pageIndex - 1],
+      pageIndex
+    }
+  }, [pageIndex, hasPreviousPage, hasNextPage, data?.pages])
 
   return (
-    <ThingsTable data={data?.payload} isFetching={isFetching} error={error} />
+    <ThingsTable
+      data={data?.pages[pageIndex]}
+      pagination={pagination}
+      isFetching={isFetching}
+      error={error}
+      onPaginationChange={onPaginationChange}
+    />
   )
 }
 
