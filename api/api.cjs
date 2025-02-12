71f577284f93846b1ed4bda951a5439b20bc775a
lalli-oni <lalli.oni@gmail.com>
2025-02-12 11:19:51 +0100

    Add pagination to api

--- api/api.cjs
+++ api/api.cjs
@@ -9,8 +9,16 @@ const data = require('./data.json')
 app.use(cors())
 
 app.get('/', (req, res) => {
+  // Id/Title of the first item in page
+  // TODO (LTJ): Handle invalid pagination values
+  // TODO (LTJ): Controllable page size
+  const startCursor = req.params.cursor
+
+  const startIndex = Math.max(data.findIndex((d) => d.title === startCursor), 0)
+  const endIndex = startIndex + 10
+  console.log(`${startIndex}-${endIndex}`)
   setTimeout(() => {
-    res.send(data.slice(0, 3))
+    res.send(data.slice(startIndex, endIndex))
   }, 3000)
 })
 
