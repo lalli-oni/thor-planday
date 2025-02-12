00cb79142c088aab06d10a9d1c45b00d4624ee2a
lalli-oni <lalli.oni@gmail.com>
2025-02-12 14:18:47 +0100

    Remove redundant query

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,3 @@
-import { useQuery } from "@tanstack/react-query"
-
 export interface QueryResponse<T> {
   payload: Array<T>;
   meta: {
@@ -14,15 +12,5 @@ export interface Thing {
 	imagePath: string;
 }
 
-function useThings() {
-  return useQuery({
-    queryKey: ['thing'],
-    retry: false,
-    queryFn: async (): Promise<Response<Thing>> => {
-      const response = await fetch('http://localhost:3000')
-      return await response.json()
-    },
-  })
-}
-
-export { useThings }
+// NOTE (LTJ): This file is now only type declaration for DTO
+//  But a suitable housing for facade query hooks
