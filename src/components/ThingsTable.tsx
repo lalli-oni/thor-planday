b71e35038c40f966e6eea96296415e9ba7c8bb64
lalli-oni <lalli.oni@gmail.com>
2025-02-13 14:43:25 +0100

    Style table Style top bar Add spacing Prevent horizontal overflow Add sizing to table images

--- src/components/ThingsTable.tsx
+++ src/components/ThingsTable.tsx
@@ -1,4 +1,5 @@
 import { createColumnHelper, flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
+import styled from "styled-components";
 
 import { type QueryResponse, type Thing } from "../api";
 
@@ -12,6 +13,27 @@ export interface ThingsTableProps {
   pagination: { hasNextPage: boolean; hasPreviousPage: boolean; pageIndex: number; };
 }
 
+const TableContainer = styled.div`
+  height: 100%;
+  padding: 3rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`
+
+const TopBar = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+const ActionBar = styled.div`
+  display: flex;
+  gap: 0.5rem;
+`
+
+const Image = styled.img`
+  max-height: 10rem;
+`
 
 function ThingsTable(props: ThingsTableProps) {
   const { data, isFetching, error, onPaginationChange, pagination } = props
@@ -25,7 +47,7 @@ function ThingsTable(props: ThingsTableProps) {
     }),
     columnHelper.accessor('imagePath', {
       header: () => (<span>Image</span>),
-      cell: info => (<img src={info.getValue()} />)
+      cell: info => (<Image src={info.getValue()} />)
     }),
   ]
 
@@ -36,25 +58,28 @@ function ThingsTable(props: ThingsTableProps) {
   })
 
   return (
-    <>
-      <div>
-        <input type="button"
-          disabled={onPaginationChange === undefined || !pagination.hasPreviousPage}
-          onClick={() => {
-              if (onPaginationChange) onPaginationChange("backward")
-          }}
-          value="Previous"
-        />
-        <input type="button"
-          disabled={onPaginationChange === undefined || !pagination.hasNextPage}
-          onClick={() => {
-              if (onPaginationChange) onPaginationChange("forward")
-          }}
-          value="Next"
-        />
-        <span>Showing {data?.payload?.length} rows of page {pagination.pageIndex}</span>
-        <span>{data?.meta?.previousCursor} {data?.meta?.nextCursor}</span>
-      </div>
+    <TableContainer>
+      <TopBar>
+        <ActionBar>
+          <input type="button"
+            disabled={onPaginationChange === undefined || !pagination.hasPreviousPage}
+            onClick={() => {
+                if (onPaginationChange) onPaginationChange("backward")
+            }}
+            value="Previous"
+          />
+          <input type="button"
+            disabled={onPaginationChange === undefined || !pagination.hasNextPage}
+            onClick={() => {
+                if (onPaginationChange) onPaginationChange("forward")
+            }}
+            value="Next"
+          />
+        </ActionBar>
+        <div>
+          <span>{!isFetching && !error ? `${data?.payload?.length} rows of page ${pagination.pageIndex + 1}` : '. . .'}</span>
+        </div>
+      </TopBar>
       <table>
         <thead>
           {table.getHeaderGroups().map(headerGroup => (
@@ -73,7 +98,7 @@ function ThingsTable(props: ThingsTableProps) {
           ))}
         </thead>
         <tbody>
-          {data?.payload?.length ? 
+          {data?.payload?.length ?
             table.getRowModel().rows.map(row => (
               <tr key={row.id}>
                 {row.getVisibleCells().map(cell => (
@@ -89,7 +114,7 @@ function ThingsTable(props: ThingsTableProps) {
       {!isFetching && !error && !data?.payload?.length ? <div>no results</div> : null}
       {isFetching ? <div>Loading...</div> : null}
       {error ? <div>Error while loading data! {error.message}</div> : null}
-    </>
+    </TableContainer>
   )
 }
 
