c480f0ee82a96287ef85678743dc99c7cadbf98f
lalli-oni <lalli.oni@gmail.com>
2025-02-13 15:55:22 +0100

    [fix] Incorrect component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-import Table from './components/common/organisms/Table'
+import ThingsTable from './components/ThingsTable'
 
 const queryClient = new QueryClient()
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Table />
+      <ThingsTable />
     </QueryClientProvider>
   )
 }
